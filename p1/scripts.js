39c4033ea23b2eb2b09f2fe8357c8f0154c81216
Gabe <gabe@Gabes-MacBook-Pro.local>
2019-10-06 16:02:47 -0400

    Beautify files

--- p1/scripts.js
+++ p1/scripts.js
@@ -2,54 +2,53 @@ let app = new Vue({
   el: "#root",
   data: {
     playerChoice: null,
-		activeChoice: null,
-		computerChoice: null,
+    activeChoice: null,
+    computerChoice: null,
     winner: null
   },
   methods: {
-		mixit: function(){
-		this.compare(this.computerChoice, this.playerChoice)
-		},
+    mixit: function() {
+      this.compare(this.computerChoice, this.playerChoice);
+    },
     compare: function(a, b) {
       if (a === b) {
-        this.winner = 'tie';
+        this.winner = "tie";
       } else {
-				if (a === "rock") {
-					if (b === "scissors") {
-						this.winner = "rock";
-					} else {
-						this.winner = "paper";
-					}
-				}
-				if (a === "paper") {
-					if (b === "rock") {
-						this.winner = "paper";
-					} else {
-						this.winner = "scissors";
-					}
-				}
-				if (a === "scissors") {
-					if (b === "rock") {
-						this.winner = "rock";
-					} else {
-						this.winner = "scissors";
-					}
-				}
-
-			}
+        if (a === "rock") {
+          if (b === "scissors") {
+            this.winner = "rock";
+          } else {
+            this.winner = "paper";
+          }
+        }
+        if (a === "paper") {
+          if (b === "rock") {
+            this.winner = "paper";
+          } else {
+            this.winner = "scissors";
+          }
+        }
+        if (a === "scissors") {
+          if (b === "rock") {
+            this.winner = "rock";
+          } else {
+            this.winner = "scissors";
+          }
+        }
+      }
     },
     compChoice: function() {
       let compAnswer = Math.random();
       if (compAnswer < 0.33) {
-				this.computerChoice = "rock";
+        this.computerChoice = "rock";
       } else if (compAnswer <= 0.66) {
-				this.computerChoice = "paper";
+        this.computerChoice = "paper";
       } else {
-				this.computerChoice = "scissors";
-			}
+        this.computerChoice = "scissors";
+      }
     }
-	},
-	beforeMount(){
-    this.compChoice()
- },
+  },
+  beforeMount() {
+    this.compChoice();
+  }
 });
