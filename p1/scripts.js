9e590a6637eb1f88ba1696a111f1e9feaea7310b
Gabe <gabe@Gabes-MacBook-Pro.local>
2019-10-06 16:00:41 -0400

    Continue progress on Project 1

--- p1/scripts.js
+++ p1/scripts.js
@@ -1,16 +1,55 @@
 let app = new Vue({
-	el: '#root',
-	data:{
-		playerChoice: null,
+  el: "#root",
+  data: {
+    playerChoice: null,
 		activeChoice: null,
-	}
-});
+		computerChoice: null,
+    winner: null
+  },
+  methods: {
+		mixit: function(){
+		this.compare(this.computerChoice, this.playerChoice)
+		},
+    compare: function(a, b) {
+      if (a === b) {
+        this.winner = 'tie';
+      } else {
+				if (a === "rock") {
+					if (b === "scissors") {
+						this.winner = "rock";
+					} else {
+						this.winner = "paper";
+					}
+				}
+				if (a === "paper") {
+					if (b === "rock") {
+						this.winner = "paper";
+					} else {
+						this.winner = "scissors";
+					}
+				}
+				if (a === "scissors") {
+					if (b === "rock") {
+						this.winner = "rock";
+					} else {
+						this.winner = "scissors";
+					}
+				}
 
-let compAnswer = Math.random();
-if (compAnswer < 0.33) {
-	compAnswer = "rock";
-} else if (compAnswer <= 0.66) {
-	compAnswer = "paper";
-} else {
-	compAnswer = "scissors";
-}
+			}
+    },
+    compChoice: function() {
+      let compAnswer = Math.random();
+      if (compAnswer < 0.33) {
+				this.computerChoice = "rock";
+      } else if (compAnswer <= 0.66) {
+				this.computerChoice = "paper";
+      } else {
+				this.computerChoice = "scissors";
+			}
+    }
+	},
+	beforeMount(){
+    this.compChoice()
+ },
+});
