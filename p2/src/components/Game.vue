ff5a620f6b74fa2462eedbb88d2c23e15f064258
Gabe <gabe@Gabes-MacBook-Pro.local>
2019-10-29 21:56:03 -0400

    Game working

--- p2/src/components/Game.vue
+++ p2/src/components/Game.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<div class="grid">
   <button
     class="card"
     id="rock"
@@ -24,9 +24,33 @@
 
 <script>
 export default {
-  data() {
-    return {
-    };
-  }
-};
+}
 </script>
+
+<style scoped>
+#rock {
+  opacity: 0;
+  background: #e9724c;
+  border: 3px solid #c96341;
+  animation: intro 0.5s ease-in-out 0.3s 1 forwards;
+}
+
+#paper {
+  opacity: 0;
+  background: #0fa3b1;
+  border: 3px solid #0a7781;
+  animation: intro 0.5s ease-in-out 0.6s 1 forwards;
+}
+
+#scissors {
+  opacity: 0;
+  background: #ffc857;
+  border: 3px solid #da950b;
+  animation: intro 0.5s ease-in-out 0.9s 1 forwards;
+}
+
+.isActive {
+  transform: scale(1.08);
+  box-shadow: 0 14px 28px #00000040, 0 10px 10px #00000038;
+}
+</style>
